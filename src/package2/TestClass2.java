082dd539e95b9a7ac2f3fb402d2fa4d7b926d2b3
hyungyong choi <zkfhtmfp@naver.com>
2022-02-03 10:43:43 +0900

    This is frist testing!

---
+++ src/package2/TestClass2.java
@@ -0,0 +1,10 @@
+package package2;
+
+public class TestClass2 {
+
+		public static void main(String[] args) {
+			
+			System.out.println("hello! This is Testing!");
+			//Githob connecting test!
+		}
+}
